f99942406457f80889dd411d47313001a35e7943
KESHAV GUPTA <59222869+keshavgupta848101@users.noreply.github.com>
2021-03-28 14:07:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -42,42 +42,6 @@ function todo(){
         }
     }
 }
-// btn.onclick = function (e) {
-//     if (inputText.value.length === 0) {
-//         return false;
-//     }
-
-//     else {
-//         const todoItem = inputText.value;
-
-//         let li = document.createElement('li');
-//         li.innerText = todoItem;
-//         ul.appendChild(li);
-//         inputText.value = "";
-
-//         var dBtn = document.createElement("button");
-//         dBtn.appendChild(document.createTextNode("X"));
-//         li.appendChild(dBtn);
-//         dBtn.addEventListener("click", deleteListItem);
-
-//         function deleteListItem() {
-//               li.remove();
-//         }
-        
-//         var edit = document.createElement("button");
-//         edit.appendChild(document.createTextNode("Edit"));
-//         li.appendChild(edit);
-//         edit.addEventListener("click", editLi);
-
-//         function editLi() {
-//             let p = prompt("Edit Your Entry");
-//             li.innerText = p;
-//             li.appendChild(dBtn);
-//             li.appendChild(edit);
-
-//         }
-//     }
-// }
 
 // Date code 
 
